ddd691a4ae8646eb36323b57009501e6f7412d5d
Daniel Scharp <daniel.scharp@hotmail.com>
2024-06-28 17:12:52 -0300

    carrossel

---
+++ carrossel/SwiperJs/swiper-3d-slider/README.md
@@ -0,0 +1,4 @@
+# Swiper-3D slider
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/shamim539/pen/vdaqQG](https://codepen.io/shamim539/pen/vdaqQG).
+
---
+++ carrossel/SwiperJs/swiper-3d-slider/dist/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper( '.swiper-container.two', {
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		effect: 'coverflow',
+		loop: true,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		coverflow: {
+			rotate: 0,
+			stretch: 100,
+			depth: 15,
+			modifier: 10,
+			slideShadows : true,
+		}
+} );
